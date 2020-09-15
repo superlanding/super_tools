a1dc2a998f2e00af184c27197398d3025d82e04f
clare <nn90121@gmail.com>
2020-09-15 19:20:13 +0800

    Fix SuperForm::AtomicSave callback can use block

--- lib/super_interaction/engine.rb
+++ lib/super_interaction/engine.rb
@@ -1,5 +1,7 @@
 module SuperInteraction
-  class Engine < Rails::Engine
-    paths["app/views"] = "lib/super_interaction/views"
+  if defined?(Rails::Engine)
+    class Engine < Rails::Engine
+      paths["app/views"] = "lib/super_interaction/views"
+    end
   end
 end
