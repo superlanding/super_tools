195c3ef8fa143a393d4b23d50755229aacc45b36
eddie <eddie.li.624@gmail.com>
2021-01-25 18:52:17 +0800

    FIX

--- lib/super_interaction/layout.rb
+++ lib/super_interaction/layout.rb
@@ -1,16 +1,28 @@
 module SuperInteraction
   module Layout
-    mattr_accessor :framework
-    self.framework = :bootstrap3
+
+    FRAMEWORKS = [ :bootstrap3, :bootstrap4, :beyond ]
+
+    def self.framework=(f)
+      raise "Not support framework: #{f}" if FRAMEWORKS.include?(f) == false
+      @@framework = f
+    end
+
+    def self.framework
+      @@framework
+    end
 
     def self.modal_layout
-      case framework
+      case @@framework
       when :bootstrap3
         "modal_bs3.haml"
       when :bootstrap4
         "modal_bs4.haml"
       when :beyond
         "modal_beyond.haml"
+      else
+        # Default
+        "modal_bs3.haml"
       end
     end
   end
