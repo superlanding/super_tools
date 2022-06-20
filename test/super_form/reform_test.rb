35eea175010a8e41a5225cdfbcbe0ef2b424766d
kmsheng <kmsh3ng@gmail.com>
2022-06-20 16:05:55 +0800

    update reform_test

--- test/super_form/reform_test.rb
+++ test/super_form/reform_test.rb
@@ -1,5 +1,8 @@
 require "test_helper"
 require "action_controller"
+require "reform/rails"
+
+Reform::Rails::Railtie.active_model!
 
 describe "SuperForm::ReformTest" do
 
@@ -10,11 +13,15 @@ describe "SuperForm::ReformTest" do
     i18n_prefix :wow_prefix
   end
 
+  class ReformWithFormName < SuperForm::Reform
+    form_name :one_ok_form
+  end
+
   before do
     @params = ActionController::Parameters.new({}).permit()
   end
 
-  describe "form_name" do
+  describe "i18n_prefix" do
 
     should "have default i18n_scope" do
       form = EmptyReform.new @params
@@ -24,6 +31,19 @@ describe "SuperForm::ReformTest" do
     should "have set i18n_scope" do
       form = I18nPrefixReform.new @params
       assert_equal form.class.i18n_scope, :wow_prefix
+    end
+  end
+
+  describe "form_name" do
+
+    should "have default model_name" do
+      form = EmptyReform.new @params
+      assert_equal form.class.model_name.to_s, "EmptyReform"
+    end
+
+    should "have set form_name" do
+      form = ReformWithFormName.new @params
+      assert_equal form.class.model_name.to_s, "OneOkForm"
     end
   end
 
