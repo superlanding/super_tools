25f12d5d5a8d4224ba5ecd855ac3e80759fae5ac
kmsheng <kmsh3ng@gmail.com>
2022-06-20 16:04:59 +0800

    add new message to be ignored

--- test/warning_ignore.rb
+++ test/warning_ignore.rb
@@ -14,4 +14,12 @@ Gem.path.each do |path|
   # https://github.com/superlanding/landing/issues/1640#issuecomment-1158456627
   Warning.ignore(/rails_test_unit_reporter_patch.rb:\d+: warning: method redefined; discarding old format_rerun_snippet/, path)
   Warning.ignore(/reporter.rb:\d+: warning: previous definition of format_rerun_snippet was here/, path)
+
+  # https://github.com/superlanding/landing/issues/1640#issuecomment-1160104446
+  Warning.ignore(/inheritable_attr.rb:\d+: warning: method redefined; discarding old model_options=/, path)
+  Warning.ignore(/inheritable_attr.rb:\d+: warning: previous definition of model_options= was here/, path)
+  Warning.ignore(/inheritable_attr.rb:\d+: warning: method redefined; discarding old model_options/, path)
+  Warning.ignore(/inheritable_attr.rb:\d+: warning: previous definition of model_options was here/, path)
+  Warning.ignore(/reform\/form\/active_model.rb:\d+: warning: method redefined; discarding old to_model/, path)
+  Warning.ignore(/reform\/form\/active_model.rb:\d+: warning: previous definition of to_model was here/, path)
 end
