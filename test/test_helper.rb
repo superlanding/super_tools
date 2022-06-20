9e073d4cfbf21af59d64527b9a21408452f945a8
kmsheng <kmsh3ng@gmail.com>
2022-06-20 19:38:04 +0800

    add db setup to test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,36 @@
+ENV['RAILS_ENV'] = 'test'
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+
+require "active_model/railtie"
+
+module Dummy
+  class Application < Rails::Application
+    config.eager_load = false
+    config.active_support.deprecation = :stderr
+
+    if config.respond_to?(:active_model)
+      config.active_model.i18n_customize_full_message = true
+    end
+  end
+end
+
+Dummy::Application.initialize!
+
+require "active_record"
+
+class Book < ActiveRecord::Base
+end
+
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: ':memory:')
+ActiveRecord::Schema.verbose = false
+
+load "#{File.dirname(__FILE__)}/support/schema.rb"
 
 require "warning_ignore"
 require "minitest/autorun"
 require "super_tools"
 require "shoulda-context"
-
-# HELPERS
 require "helpers/build_orders_helper"
-require "helpers/db_helper"
-
 require "reform/rails"
+
 Reform::Rails::Railtie.active_model!
