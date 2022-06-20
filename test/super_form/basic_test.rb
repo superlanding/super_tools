58e9b31021d2cf3b806d472c2e40510fa1ab4acb
kmsheng <kmsh3ng@gmail.com>
2022-06-20 11:52:39 +0800

    re-style

--- test/super_form/basic_test.rb
+++ test/super_form/basic_test.rb
@@ -3,6 +3,13 @@ require "action_controller"
 
 describe "SuperForm::BasicTest" do
 
+  class EmptyForm < SuperForm::Basic
+  end
+
+  class FormWithFormName < SuperForm::Basic
+    form_name :my_customized_form_name
+  end
+
   before do
     @params = ActionController::Parameters.new({ one: 'one', two: 'two' })
       .permit(:one, :two)
@@ -11,16 +18,11 @@ describe "SuperForm::BasicTest" do
   describe "form_name" do
 
     should "have default model_name" do
-      class EmptyForm < SuperForm::Basic
-      end
       form = EmptyForm.new @params
       assert_equal(form.model_name, 'EmptyForm')
     end
 
     should "have set form_name" do
-      class FormWithFormName < SuperForm::Basic
-        form_name :my_customized_form_name
-      end
       form = FormWithFormName.new @params
       assert_equal(form.model_name, 'MyCustomizedFormName')
     end
