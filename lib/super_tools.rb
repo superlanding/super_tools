dd69b2fe8b55fdcd16976e764a151e4fac6769cb
kmsheng <kmsh3ng@gmail.com>
2022-06-20 10:22:42 +0800

    re-style

--- lib/super_tools.rb
+++ lib/super_tools.rb
@@ -3,44 +3,56 @@ require "active_support"
 require "active_model"
 require "active_record"
 require "action_view"
+
 # VERSION
 require "super_tools/version"
+
 # TASKS
 require "super_process/core"
+
 # FORMS
 require "super_spreadsheet/loader"
 require "super_form/basic"
 require "super_form/reform"
+
 # TABLES
 require "super_table/action_view"
 require "super_table/builder"
 require "super_table/record"
 require "super_table/tableable"
 require "super_table/view_helpers"
+
 # FORMATTER
 require "super_formatter/import"
 require "super_formatter/row"
+
 # FORMATTER (ECAN)
 require "super_formatter/ecan/head"
 require "super_formatter/ecan/row"
 require "super_formatter/ecan/import"
+
 # FORMATTER (TCAT)
 require "super_formatter/tcat/head"
 require "super_formatter/tcat/row"
 require "super_formatter/tcat/import"
+
 # FORMATTER (HCT)
 require "super_formatter/hct/head"
 require "super_formatter/hct/row"
 require "super_formatter/hct/import"
+
 # FORMATTER (SHOPLINE)
 require "super_formatter/shopline/head"
 require "super_formatter/shopline/row"
 require "super_formatter/shopline/order"
 require "super_formatter/shopline/import"
+
 # SEARCH
 require "super_search/scroll"
+
 # ZIP CODE
 require "super_zipcode/taiwan"
+
 # INTERACTION
 require "super_interaction/layout"
 require "super_interaction/engine"
@@ -48,8 +60,10 @@ require "super_interaction/beyond"
 require "super_interaction/beyond_helper"
 require "super_interaction/bootstrap"
 require "super_interaction/bootstrap_helper"
+
 # FORMATTER
 require "super_logger/formatter"
+
 # Values
 require "super_value/base"
 
