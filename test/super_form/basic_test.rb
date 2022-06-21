50c1e3635391f858700aea375c34b691bd580db6
kmsheng <kmsh3ng@gmail.com>
2022-06-21 11:12:16 +0800

    rm before block from basic_test

--- test/super_form/basic_test.rb
+++ test/super_form/basic_test.rb
@@ -21,19 +21,15 @@ describe "SuperForm::BasicTest" do
     end
   end
 
-  before do
-    @params = ActionController::Parameters.new({}).permit()
-  end
-
   describe "form_name" do
 
     should "have default model_name" do
-      form = EmptyForm.new @params
+      form = EmptyForm.new
       assert_equal form.model_name, "EmptyForm"
     end
 
     should "have set form_name" do
-      form = FormWithFormName.new @params
+      form = FormWithFormName.new
       assert_equal form.model_name, "MyCustomizedFormName"
     end
   end
@@ -42,13 +38,13 @@ describe "SuperForm::BasicTest" do
 
     # https://github.com/rails/rails/blob/v6.1.6/activemodel/lib/active_model/error.rb#L38
     should "have default i18n_scope" do
-      form = EmptyForm.new @params
+      form = EmptyForm.new
       assert_equal form.class.i18n_scope, :forms
       assert_equal form.model_name.i18n_key, :empty_form
     end
 
     should "be able to set i18n_scope" do
-      form = I18nScopeForm.new @params
+      form = I18nScopeForm.new
       assert_equal form.class.i18n_scope, :customized_i18n_prefix
       assert_equal form.model_name.i18n_key, :customized_form_name
     end
@@ -59,7 +55,7 @@ describe "SuperForm::BasicTest" do
     # https://github.com/rails/rails/blob/v6.1.6/activemodel/lib/active_model/naming.rb#L164
     # SuperForm::Basic 的 active_model_name_for 把 namespace 強制設定成 nil
     should "leave namespace untouched" do
-      form = My::Form.new @params
+      form = My::Form.new
       assert_equal form.model_name.param_key, "my_form"
     end
   end
