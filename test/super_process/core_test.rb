29243c66e79d4208bd369d37cff2c7b3cd1cd9cc
kmsheng <kmsh3ng@gmail.com>
2022-06-22 15:06:37 +0800

    update core_test.rb

--- test/super_process/core_test.rb
+++ test/super_process/core_test.rb
@@ -6,18 +6,25 @@ class SuperProcessCoreTest < MiniTest::Spec
     init :book do
       attribute :heading, String
     end
+    attribute :subheading, String
+  end
+
+  before do
+    @book = Book.new(name: "Shitcode in a nutshell")
+    @cover = BookCover.new @book
   end
 
   describe "init" do
     should "set model_name to attr_accessor and run block" do
-      book = Book.new(name: "Shitcode in a nutshell")
-      cover = BookCover.new book
+      assert_equal @cover.book, @book
+      assert @cover.respond_to? :book=
 
-      assert_equal cover.book, book
-      assert cover.respond_to? :book=
+      assert @cover.respond_to? :heading
+      assert @cover.respond_to? :heading=
 
-      assert cover.respond_to? :heading
-      assert cover.respond_to? :heading=
+      assert @cover.respond_to? :subheading
+      assert @cover.respond_to? :subheading=
     end
   end
+
 end
