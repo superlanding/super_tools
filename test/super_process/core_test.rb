4f64d98c57ca61b48efb5c430caf091c57cdbe5e
kmsheng <kmsh3ng@gmail.com>
2022-06-22 15:50:34 +0800

    update core_test.rb

--- test/super_process/core_test.rb
+++ test/super_process/core_test.rb
@@ -7,11 +7,43 @@ class SuperProcessCoreTest < MiniTest::Spec
       attribute :heading, String
     end
     attribute :subheading, String
+    attribute :logs, Array
+
+    validate :validate_book_name
+
+    before_call :before_call
+
+    callable do
+      logs.push :callable
+    end
+
+    def before_call
+      logs.push :before_call
+    end
+
+    def after_call
+      logs.push :after_call
+    end
+
+    def before_task
+      logs.push :before_task
+    end
+
+    def after_task
+      logs.push :after_task
+    end
+
+    def validate_book_name
+      # 不可以髒髒
+      errors.add(:name, "Invalid book name") if book.name.include?("Shit")
+    end
   end
 
   before do
     @book = Book.new(name: "Shitcode in a nutshell")
-    @cover = BookCover.new @book
+    @cover = BookCover.new(@book).tap do |cover|
+      cover.logs = []
+    end
   end
 
   describe "init" do
@@ -27,4 +59,15 @@ class SuperProcessCoreTest < MiniTest::Spec
     end
   end
 
+  describe "callable" do
+
+    should "be blocked by invalid book name" do
+      assert_raises SuperProcess::Core::ValidError do
+        @cover.call!
+      end
+      # validation 沒過，所以只有 :before_call
+      assert_equal @cover.logs, [:before_call]
+    end
+
+  end
 end
