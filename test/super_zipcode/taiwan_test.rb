5c3e4390c2e0c54b1beeb4e93878399d13015f56
kmsheng <kmsh3ng@gmail.com>
2022-06-23 16:21:11 +0800

    add super_zipcode/taiwan_test.rb

---
+++ test/super_zipcode/taiwan_test.rb
@@ -0,0 +1,22 @@
+require "test_helper"
+
+class SuperZipcodeTaiwanTest < MiniTest::Spec
+
+  describe "find_zip_code" do
+
+    should "handle invalid address" do
+      assert_nil SuperZipcode::Taiwan.find_zip_code("亂打地址")
+    end
+
+    SuperZipcode::Taiwan::CITY_ZONE_ZIP_CODES.each do |city, district_hash|
+      district_hash.each do |district, code|
+        address = "#{city}#{district}"
+        should "handle address: #{address}" do
+          assert_equal SuperZipcode::Taiwan.find_zip_code(address), code
+        end
+      end
+    end
+
+  end
+
+end
