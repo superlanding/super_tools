082b6aac45011669805b2d801eb96ff3378da277
kmsheng <kmsh3ng@gmail.com>
2022-06-23 12:51:06 +0800

    update super_spreadsheet/loader_test.rb

--- test/super_spreadsheet/loader_test.rb
+++ test/super_spreadsheet/loader_test.rb
@@ -3,6 +3,12 @@ require "action_controller"
 
 class SuperSpreadsheetLoaderTest < MiniTest::Spec
 
+  def parse(path)
+    spreadsheet = SuperSpreadsheet::Loader.new "test/support/spreadsheet/#{path}"
+    spreadsheet.call
+    spreadsheet
+  end
+
   describe "SuperSpreadsheet::Loader" do
 
     should "have attr_accessor :file_path" do
@@ -12,15 +18,18 @@ class SuperSpreadsheetLoaderTest < MiniTest::Spec
     end
 
     should "handle invalid file extension" do
-      spreadsheet = SuperSpreadsheet::Loader.new "test/support/spreadsheet/file.lol"
-      spreadsheet.call
+      spreadsheet = parse "file.lol"
       assert spreadsheet.errors.first.full_message, "File 檔案格式錯誤"
     end
 
     should "handle empty csv file" do
-      spreadsheet = SuperSpreadsheet::Loader.new "test/support/spreadsheet/empty.csv"
-      spreadsheet.call
+      spreadsheet = parse "empty.csv"
       assert spreadsheet.errors.first.full_message, "檔案內容錯誤，空白檔案"
+    end
+
+    should "parse utf8 csv" do
+      spreadsheet = parse "utf8.csv"
+      assert spreadsheet.rows, [["姓名", "電話"], ["許功蓋", "0912-333-456"]]
     end
 
   end
