b1e4e2dea1f7f41626f9ffa93d4117a0ec934dcf
Eddie Li (ADZ) <eddie.li.624@gmail.com>
2022-07-20 03:41:24 +0800

    FIX

--- lib/super_form/reform.rb
+++ lib/super_form/reform.rb
@@ -1,23 +1,3 @@
-# Hack Dry::Type 升級 0.15 以後
-# 變成強制型別，會造成 params input 空字串時的錯誤
-# 這裡是 hack
-# https://github.com/apotonick/disposable/blob/master/lib/disposable/twin/coercion.rb
-# https://github.com/dry-rb/dry-types/blob/a4983c88299b6f323a769f783cf956629e61f8ed/lib/dry/types/coercions/params.rb#L86
-
-Dry::Types::Coercions::Params.module_eval do
-  def self.to_int(input, &block)
-    if input.is_a? String
-      Integer(input, 10)
-    else
-      Integer(input)
-    end
-  rescue ArgumentError, TypeError => e
-    return input.to_i if input.respond_to?(:to_i)
-
-    CoercionError.handle(e, &block)
-  end
-end
-
 require "super_form/concerns/atomic_save"
 require "reform"
 require "reform/rails"
@@ -48,5 +28,25 @@ class SuperForm::Reform < Reform::Form
     end
     # NOTE: 這行很可能沒有作用
     model(name)
+  end
+end
+
+# Hack Dry::Type 升級 0.15 以後
+# 變成強制型別，會造成 params input 空字串時的錯誤
+# 這裡是 hack
+# https://github.com/apotonick/disposable/blob/master/lib/disposable/twin/coercion.rb
+# https://github.com/dry-rb/dry-types/blob/a4983c88299b6f323a769f783cf956629e61f8ed/lib/dry/types/coercions/params.rb#L86
+
+Dry::Types::Coercions::Params.module_eval do
+  def self.to_int(input, &block)
+    if input.is_a? String
+      Integer(input, 10)
+    else
+      Integer(input)
+    end
+  rescue ArgumentError, TypeError => e
+    return input.to_i if input.respond_to?(:to_i)
+
+    CoercionError.handle(e, &block)
   end
 end
